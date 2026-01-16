7513a985b14b08f8a27cf02e15a0809b9878d667
Ankit Mahato <mahatoankit0007@gmail.com>
2026-01-16 21:52:21 +0530

    Add files via upload

---
+++ 2.Insert into Table/insert into table.sql
@@ -0,0 +1,22 @@
+--Create a Table 
+
+Create Table Movies (
+        Name VARCHAR(100) PRIMARY KEY,
+        ActorName VARCHAR(100),
+        ProducerName VARCHAR(100),
+        Budget INT,
+        IMDBRating FLOAT
+
+
+);
+
+
+INSERT INTO Movies (Name, ActorName, ProducerName, Budget, IMDBRating)
+VALUES 
+('Inception', 'Leonardo DiCaprio', 'Emma Thomas', 160000000, 8.8),
+('Interstellar', 'Matthew McConaughey', 'Christopher Nolan', 165000000, 8.6),
+('The Dark Knight', 'Christian Bale', 'Charles Roven', 185000000, 9.0);
+
+
+SELECT * FROM Movies;
+

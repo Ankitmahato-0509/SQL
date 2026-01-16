6aea5da950bb1d84bf7fc4e6df949b037b43690e
Ankit Mahato <mahatoankit0007@gmail.com>
2026-01-16 21:38:43 +0530

    Add files via upload

---
+++ 1.Create Table/Create Table.sql
@@ -0,0 +1,12 @@
+--Create a Table 
+
+Create Table Movies (
+        Name VARCHAR(100) PRIMARY KEY,
+        ActorName VARCHAR(100),
+        ProducerName VARCHAR(100),
+        Budget INT,
+        IMDBRating FLOAT
+
+
+);
+
